f953bbbda3b5cb164488ee4f54037d06d9ad9cd8
Shivam B <shivambhatia@outlook.com>
2021-08-28 16:35:40 +0530

    payment changed from usd to inr and changed config

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-if(process.env.NODE_ENV !== 'production') require('dotenv').config({path: 'C:\Users\Shivam Bhatia\Desktop\Web Devloper Course\Projects\WEB Dev Projects\crwn-clothing\.env'});
+if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
-app.use(cors());
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'client/build')));
